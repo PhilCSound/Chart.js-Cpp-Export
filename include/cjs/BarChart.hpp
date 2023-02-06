4054151363a8ebcc4750a4eb3851a8621bf44293
PhilC <yru2kind@gmail.com>
2023-02-05 22:25:48 -0600

    Removed .cpp file. Made functions inline.

--- include/cjs/BarChart.hpp
+++ include/cjs/BarChart.hpp
@@ -7,26 +7,48 @@ class BarChart
     public:
         BarChart() = default;
 
-        void AddBarChartDataset(const BarChartDataset<NumericType>& dataset);
+        inline void AddBarChartDataset(const BarChartDataset<NumericType>& dataset)
+            { m_barChartDatasets.push_back(dataset); }
         
-        void MakeGraphHorizontal();
-        void MakeGraphVertical();
+        inline void MakeGraphHorizontal() { m_isHorizontal = true; }
+        inline void MakeGraphVertical() { m_isHorizontal = false; }
 
-        void SetGrouped(const bool isGrouped);
-        void SetLabels(const std::vector<std::string>& labels); 
-        void SetSkipNull(const bool skipNull);
-        //void SetStack()  THis is a dataset thing
+        inline void SetGrouped(const bool isGrouped) { m_isGrouped = false; }
+        inline void SetLabels(const std::vector<std::string>& labels) { m_labels = label; } 
+        inline void SetSkipNull(const bool skipNull) { m_skipNull = skipNull; }
+        //void SetStack()  This is a dataset thing
         //void Order() Dataset order of drawing
         
-        
+
         //Operator overloads  
         template <typename NumericType>
-        friend std::ostream& operator<<(std::ostream& os, BarChart<NumericType>& chart);
+        inline friend std::ostream& operator<<(std::ostream& os, BarChart<NumericType>& chart)
+        {
+            if (chart.m_barChartDatasets.empty() || chart.m_labels.empty())
+                return os;
+    
+            ///TODO: This part can be moved to a HTMLDocument class.
+            os << "<!DOCTYPE html><html><div><canvas id=\"chart\"></canvas></div>"
+                << "<script src=\"https://cdn.jsdelivr.net/npm/chart.js@4.1.1/dist/chart.umd.min.js\"></script>" 
+                << "<script>const ctx = document.getElementById('chart');new Chart(ctx, {"
+                << "type: 'bar', data: { labels: [";
+            
+            //Labels
+            for(size_t i = 0; i < chart.m_labels.size(); i++)
+                os << "'" << chart.m_labels[i] << "',";
+            os << "],datasets:[{";
+            
+            for(auto& dataSet : chart.m_barChartDatasets)
+                os << dataSet.ToString();
+            os << "}});</script></html>";
+            
+            return os;
+        }
+
     private:
         std::vector<BarChartDataset<NumericType>> m_barChartDatasets;
         std::vector<std::string> m_labels;
+        bool m_isHorizontal = false;
+        bool m_isGrouped = false;
+        bool m_skipNull = false;
 };
-
-
-#pragma once
-#include "BarChart.cpp"
