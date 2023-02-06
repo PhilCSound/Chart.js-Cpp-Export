f6fef7abea4e0214f9a8518a1f5dc6e679397b9b
PhilC <yru2kind@gmail.com>
2023-02-05 22:57:44 -0600

    Tested multiple datasets.

--- main.cpp
+++ main.cpp
@@ -4,30 +4,30 @@
 #include "BarChartDataset.hpp"
 #include <iostream>
 #include <fstream>
-
+#include <algorithm>
 int main()
 {
-    Color color { 0,0, 0};
-    Color color2 {255, 255, 0};
-    Color color3 {0, 255, 255};
-
-    std::cout << "Color 1: " << color.ToHexColorCode() << '\n';
-    std::cout << "Color 2: " << color2.ToHexColorCode() << '\n'; 
-    std::cout << "Color 3: " << color3.ToHexColorCode() << '\n'; 
-    std::cout << "Test 4: " << 244 << '\n';
-
     BarChartDataset<int> barData;
+    BarChartDataset<int> barData1;
     std::vector<int> dataset;
-    for (int i = 0; i < 10; i++)
-        dataset.push_back(11-i);
+    for (int i = 0; i < 12; i++)
+        dataset.push_back(13-i);
+
     barData.SetData(dataset);
+    std::reverse(dataset.begin(), dataset.end());
+    barData1.SetData(dataset);
+
     barData.SetLabel("Test Data");
+    barData1.SetLabel("Test Data 2");
+    
     std::vector<std::string> xaxis
     {
         "Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "September", "October", "November", "December"
     };
+    
     BarChart<int> bc;
     bc.AddBarChartDataset(barData);
+    bc.AddBarChartDataset(barData1);
     bc.SetLabels(xaxis);
 
     std::ofstream outFile;
