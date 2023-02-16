c86d71e428eef0c08fb0564837a8049c03b426a3
PhilC <yru2kind@gmail.com>
2023-02-15 23:11:58 -0600

    Support for stacked/horizontal chart and begin@0

--- include/cjs/BarChart.hpp
+++ include/cjs/BarChart.hpp
@@ -13,10 +13,13 @@ class BarChart
         inline void MakeGraphHorizontal() { m_isHorizontal = true; }
         inline void MakeGraphVertical() { m_isHorizontal = false; }
 
+        inline void SetBeginAtZero(const bool isBeginAtZero) { m_beginAxisAtZero = isBeginAtZero; };
         inline void SetGrouped(const bool isGrouped) { m_isGrouped = false; }
         inline void SetLabels(const std::vector<std::string>& labels) { m_labels = labels; } 
         inline void SetSkipNull(const bool skipNull) { m_skipNull = skipNull; }
-        //void SetStack()  This is a dataset thing
+        inline void SetStackBothAxes(const bool bothAxis) {m_stackXAxis = bothAxis; m_stackYAxis = bothAxis; }
+        inline void SetStackXAxis(const bool stackXAxis) { m_stackXAxis = stackXAxis; }
+        inline void SetStackYAxis(const bool stackYAxis) { m_stackYAxis = stackYAxis; }
         //void Order() Dataset order of drawing
         
 
@@ -32,23 +35,47 @@ class BarChart
                 << "<script src=\"https://cdn.jsdelivr.net/npm/chart.js@4.1.1/dist/chart.umd.min.js\"></script>" 
                 << "<script>const ctx = document.getElementById('chart');new Chart(ctx, {"
                 << "type: 'bar', data: { labels: [";
-            
+
             //Labels
             for(size_t i = 0; i < chart.m_labels.size(); i++)
                 os << "'" << chart.m_labels[i] << "',";
-            os << "],datasets:[";
             
+            //Datasets
+            os << "],datasets:[";
+        
             for(auto& dataSet : chart.m_barChartDatasets)
                 os << "{"<< dataSet.ToString() << "},";
-            os << "]}});</script></html>";
+            os << "]},";
+
+            //Options
+            os << " options: {";
+            if(chart.m_isHorizontal)
+                os << "indexAxis: 'y',";
+            os << "scales: { x: {";     //XAxis
+            if(chart.m_stackXAxis)
+                os << "stacked: true,";
+            if(!chart.m_beginAxisAtZero && chart.m_isHorizontal)
+                os << "beginAtZero: false";
+            os << "}, y: {";     //YAxis
+            if(chart.m_stackYAxis)
+                os << "stacked: true,";
+            if(!chart.m_beginAxisAtZero && !chart.m_isHorizontal)
+                os << "beginAtZero: false";
+            os << "}}},";
+
+                //TODO: Set Grouped?
+            os << "});</script></html>";
             
             return os;
         }
 
     private:
         std::vector<BarChartDataset<NumericType>> m_barChartDatasets;
+        bool m_beginAxisAtZero = true;
         std::vector<std::string> m_labels;
         bool m_isHorizontal = false;
         bool m_isGrouped = false;
         bool m_skipNull = false;
+        bool m_stackXAxis = false;
+        bool m_stackYAxis = false;
 };
